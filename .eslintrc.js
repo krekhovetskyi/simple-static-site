0653c4f605c2c66303ef35b15075009aaadb6c30
Volodymyr Krekhovetskyi <volodymyr.krekhovetskyi@gmail.com>
2021-12-26 20:57:14 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  overrides: [],
+  rules: {
+    "object-curly-spacing": "off",
+    "comma-dangle": "off",
+    "import/no-extraneous-dependencies": "off"
+  }
+};
