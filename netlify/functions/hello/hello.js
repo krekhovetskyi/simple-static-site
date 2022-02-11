38f136674d37758c5be0dc76178534d3472066fd
Volodymyr Krekhovetskyi <volodymyr.krekhovetskyi@gmail.com>
2022-02-11 19:32:47 +0200

    simple example function, hello.js

---
+++ netlify/functions/hello/hello.js
@@ -0,0 +1,11 @@
+exports.handler = async function (event, context) {
+  console.log('Event:');
+  console.log(event);
+  console.log('Context:');
+  console.log(context);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: 'Hello World'})
+  };
+};
